7c89447bfc1935c3e171573fba756d987e3aa8a6
Anais Augu <aaugu@c2r7s5.42lausanne.ch>
2023-04-27 14:39:51 +0200

    a

--- bonus/process_bonus.c
+++ bonus/process_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 10:43:20 by aaugu             #+#    #+#             */
-/*   Updated: 2023/04/27 14:16:47 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/04/27 14:22:54 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,49 @@
 
 void	child_process(t_pipex *pipex, char *cmd, char **envp, int i);
 void	get_in_and_out(t_pipex *pipex, int i);
-int	wait_for_childs(t_pipex *pipex);
+int		wait_for_childs(t_pipex *pipex);
 
-int	process(t_pipex *pipex, char **av, char **envp)
+int	process(t_pipex *p, char **av, char **envp)
 {
 	int	exit_code;
 	int	i;
 
 	i = 0;
-	pipex->process.pids = (int *)malloc(sizeof(int) * pipex->nb_cmds);
-	if (!pipex->process.pids)
-		error_exit(pipex, "malloc", "malloc failed", EXIT_FAILURE);
-	while (i < pipex->nb_cmds)
+	p->process.pids = (int *)malloc(sizeof(int) * p->nb_cmds);
+	if (!p->process.pids)
+		error_exit(p, "malloc", "malloc failed", EXIT_FAILURE);
+	while (i < p->nb_cmds)
 	{
-		pipex->process.pids[i] = fork();
-		if (pipex->process.pids[i] < 0)
-			error_exit(pipex, "fork failed", "Resource temporarily unavailable"\
-			, 4);
-		else if (pipex->process.pids[i] == 0)
-			child_process(pipex, av[2 + pipex->heredoc + i], envp, i);
+		p->process.pids[i] = fork();
+		if (p->process.pids[i] < 0)
+			error_exit(p, "fork failed", "Resource temporarily unavailable", 4);
+		else if (p->process.pids[i] == 0)
+			child_process(p, av[2 + p->heredoc + i], envp, i);
 		i++;
 	}
-	exit_code = wait_for_childs(pipex);
+	exit_code = wait_for_childs(p);
 	return (exit_code);
 }
 
-void	child_process(t_pipex *pipex, char *cmd, char **envp, int i)
+void	child_process(t_pipex *p, char *cmd, char **envp, int i)
 {
-	get_in_and_out(pipex, i);
-	if (dup2(pipex->process.in, STDIN_FILENO) == ERROR)
+	get_in_and_out(p, i);
+	if (dup2(p->process.in, STDIN_FILENO) == ERROR || \
+		dup2(p->process.out, STDOUT_FILENO) == ERROR)
 	{
-		error_message("dupin", "Bad file descriptor");
+		error_message("dup2", "Bad file descriptor");
 		exit(errno);
 	}
-	if (dup2(pipex->process.out, STDOUT_FILENO) == ERROR)
-	{
-		error_message("dupout", "Bad file descriptor");
-		exit(errno);
-	}
-	close_pipes(pipex);
-	pipex->process.cmd_args = get_args(cmd);
-	if (!pipex->process.cmd_args)
+	close_pipes(p);
+	p->process.cmd_args = get_args(cmd);
+	if (!p->process.cmd_args)
 	{
 		error_message("malloc", "malloc failed");
 		exit(EXIT_FAILURE);
 	}
-	execve(pipex->cmds_path[i], pipex->process.cmd_args, envp);
+	execve(p->cmds_path[i], p->process.cmd_args, envp);
 	error_message(cmd, "command not found");
-	ft_strs_free(pipex->process.cmd_args, ft_strs_len(pipex->process.cmd_args));
+	ft_strs_free(p->process.cmd_args, ft_strs_len(p->process.cmd_args));
 	exit(127);
 }
 
@@ -75,18 +70,24 @@ void	get_in_and_out(t_pipex *pipex, int i)
 		pipex->process.out = pipex->fd_out;
 }
 
-int	wait_for_childs(t_pipex *pipex)
+int	wait_for_childs(t_pipex *p)
 {
 	int	i;
+	int	wpid;
 	int	status;
+	int	exit_code;
 
 	i = 0;
-	while (i < pipex->nb_cmds)
+	exit_code = 127;
+	while (i < p->nb_cmds)
 	{
-		waitpid(pipex->process.pids[i], &status, 0);
-		if (WIFEXITED(status) != 0)
-			return (WEXITSTATUS(status));
+		wpid = waitpid(p->process.pids[i], &status, 0);
+		if (wpid == p->process.pids[p->nb_cmds])
+		{
+			if (WIFEXITED(status))
+				exit_code = WEXITSTATUS(status);
+		}
 		i++;
 	}
-	return (0);
+	return (exit_code);
 }
